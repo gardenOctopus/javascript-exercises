caff688ed7647585a62d3d895815c45444ec6e06
octopusGarden <zebraspider@protonmail.com>
2022-11-07 11:39:51 -0500

    Completed Reverse String Exerecise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
+const reverseString = function(string) {
+    //Convert String to Array
+    //Reverse Array
+        return string.split('').reverse().join('').toString();
+    
+    //Join & Convert to String
+    };
 
 // Do not edit below this line
 module.exports = reverseString;
