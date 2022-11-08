02f77c95babc4050c4cfac64d346ba9ca9e6b018
octopusGarden <zebraspider@protonmail.com>
2022-11-08 09:58:27 -0500

    Completed Palindromes Exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function(string) {
+    //Split string into an array 
+    let result = string.split('');
+    //Remove spaces and punctuation
+    let die = [' ', '.', ',', '!'];
+    result = result.filter((item) => !die.includes(item));
+    //Joins forward facing array
+    let forwards = result.join('').toLowerCase();
+    //Puts array in new variable and reverses it, and makes it into a string
+    let backwards = result.reverse().join('').toLowerCase();
+    //Determines if both strings are identical
+    if (forwards === backwards) {
+        return true;
+    } else {
+        return false; 
+    }
 };
 
 // Do not edit below this line
