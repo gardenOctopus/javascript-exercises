0447857fcad8083c99ed3a5a3f1081b621985312
octopusGarden <zebraspider@protonmail.com>
2022-11-08 14:39:20 -0500

    Completed Fibonacci Exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    const fibonacciArray = ['0', '1', '1', '2', '3', '5', '8', '13', '21', '34', '55', '89', '144', '233', '377', '610', '987', '1597', '2584', '4181', '6765', '10946', '17711', '28657', '46368', '75025'];
+    if (index < 0) {
+        return 'OOPS';
+    } else {
+        let output = parseInt(fibonacciArray[index]);
+        return output;
+    }
 };
 
 // Do not edit below this line
