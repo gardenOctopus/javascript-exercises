ff212e15cbfaf180629b64f4561cb7961f9029cb
octopusGarden <zebraspider@protonmail.com>
2022-11-09 10:14:00 -0500

    Completed Get the Titles Exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books){
+    let titles = books.map((obj) => {
+        return obj.title;
+    });
+    return titles; 
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
